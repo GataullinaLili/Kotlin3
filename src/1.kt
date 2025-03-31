77b7fb54b2133e0f28beeb4688c42aa980795797
GataullinaLili <liliag00@mail.ru>
2025-03-31 23:36:27 +0300

    lab 1

---
+++ src/1.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val a = 4
+    val b = 7
+val res=a % b
+    print("Остаток от деления =  ")
+    println( res )
+
+    if (res>0)
+        println("Получилось значение больше, чем 0.")
+}
